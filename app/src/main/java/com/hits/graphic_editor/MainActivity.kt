d58f9b2f81ac7c9cf61c27cf32fa9d77e0897707
Maria Tolkacheva <mash.tolk@gmail.com>
2024-05-01 00:47:24 +0700

    well, i almost done it, i just have to fix some bugs: after 2 times denying of permission the screen is going to blink, but i'll handle it later. I also remembered that i use READ_MEDIA_IMAGES and it won't work with old versions of sdk

--- app/src/main/java/com/hits/graphic_editor/MainActivity.kt
+++ app/src/main/java/com/hits/graphic_editor/MainActivity.kt
@@ -1,28 +1,28 @@
 package com.hits.graphic_editor
 
+import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.View
-import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.core.content.FileProvider
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.hits.graphic_editor.databinding.ActivityMainBinding
+import com.hits.graphic_editor.databinding.BottomSheetBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    var fab: View? = null
+    val bottomSheetBinding: BottomSheetBinding by lazy {
+        BottomSheetBinding.inflate(layoutInflater)
+    }
+    var pickedPhoto: Uri? = null
+    var tempImageUri: Uri? = null
 
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -31,12 +31,91 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        fab = binding.fab
+
+        val dialog = BottomSheetDialog(this)
+        dialog.setContentView(bottomSheetBinding.root)
+
+        fab?.setOnClickListener {
+
+            dialog.show()
+
+            bottomSheetBinding.gallery.setOnClickListener {
+                requestPermission(Manifest.permission.READ_MEDIA_IMAGES, 1)
+            }
+
+            bottomSheetBinding.camera.setOnClickListener {
+                requestPermission(Manifest.permission.CAMERA, 2)
+            }
+        }
+
+        tempImageUri = initTempUri()
     }
 
+    private val galleryLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            uri ?: return@registerForActivityResult
+            pickedPhoto = uri
+            val message: String = pickedPhoto.toString()
+            val photoIntent = Intent(this, NewProjectActivity::class.java)
+            photoIntent.putExtra("photo", message)
+            startActivity(photoIntent)
+        }
 
-    public fun showBottomSheet (view: View) {
-        val dialog = BottomSheetDialog(this)
-        dialog.setContentView(view)
-        dialog.show()
+    private val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) {
+            //tempImageUri ?: return@registerForActivityResult
+            if(it){
+                val message: String = tempImageUri.toString()
+                val photoIntent = Intent(this, NewProjectActivity::class.java)
+                photoIntent.putExtra("photo", message)
+                startActivity(photoIntent)
+            }
+        }
+
+    private val permissionToGalleryResultLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                galleryLauncher.launch("image/*")
+            }
+        }
+
+    private val permissionToCameraResultLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                cameraLauncher.launch(tempImageUri)
+            }
+        }
+
+    private fun requestPermission(permission: String, code: Int) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                permission
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (code == 1) permissionToGalleryResultLauncher.launch(permission)
+            else permissionToCameraResultLauncher.launch(permission)
+        } else {
+            if (code == 1) galleryLauncher.launch("image/*")
+            else cameraLauncher.launch(tempImageUri)
+        }
+    }
+
+    private fun initTempUri(): Uri {
+        val tempImagesDir = File(
+            applicationContext.filesDir,
+            getString(R.string.temp_images_dir))
+
+        tempImagesDir.mkdir()
+
+        val tempImage = File(
+            tempImagesDir,
+            getString(R.string.temp_image))
+
+        return FileProvider.getUriForFile(
+            applicationContext,
+            "com.hits.graphic-editor",
+            tempImage)
     }
 }
