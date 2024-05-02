882e1e31b9279bb06be433c09739e928b36d3106
Maria Tolkacheva <mash.tolk@gmail.com>
2024-05-03 00:23:06 +0700

    tried to use tab navigation. Sth wrong with containerView

--- app/src/main/java/com/hits/graphic_editor/NewProjectActivity.kt
+++ app/src/main/java/com/hits/graphic_editor/NewProjectActivity.kt
@@ -2,10 +2,14 @@ package com.hits.graphic_editor
 
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import com.hits.graphic_editor.databinding.ActivityNewProjectBinding
+
 
 class NewProjectActivity : AppCompatActivity() {
 
@@ -14,6 +18,9 @@ class NewProjectActivity : AppCompatActivity() {
     }
 
     var pickedPhoto: Uri? = null
+    var tabLayout: TabLayout? = null
+    var tabListener: OnTabSelectedListener? = null
+    lateinit var containerView: ViewGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +31,48 @@ class NewProjectActivity : AppCompatActivity() {
 
         binding.imageView.setImageURI(pickedPhoto)
 
+        tabLayout = binding.tabLayout
+        containerView.addView(tabLayout, 0)
+
+
+        tabLayout!!.addOnTabSelectedListener(object : OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                if (tabLayout!!.selectedTabPosition == 0) {
+
+                } else if (tabLayout!!.selectedTabPosition == 1) {
+
+                } else if (tabLayout!!.selectedTabPosition == 2) {
+
+                } else if (tabLayout!!.selectedTabPosition == 3) {
+
+                } else if (tabLayout!!.selectedTabPosition == 4) {
+
+                } else if (tabLayout!!.selectedTabPosition == 5) {
+
+                } else if (tabLayout!!.selectedTabPosition == 6) {
+
+                } else if (tabLayout!!.selectedTabPosition == 7) {
+
+                } else if (tabLayout!!.selectedTabPosition == 8) {
+
+                } else if (tabLayout!!.selectedTabPosition == 9) {
+
+                }
+                containerView.removeView(tabLayout)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+
         supportActionBar?.hide()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        tabListener?.let { tabLayout?.removeOnTabSelectedListener(it) }
     }
 
 }
