323143c282b09951f69280dcb6efbebc4261dc95
Stas <pfqxbr2208@gmail.com>
2024-05-02 19:43:08 +0700

    started to create rotation

--- app/src/main/java/com/hits/graphic_editor/NewProjectActivity.kt
+++ app/src/main/java/com/hits/graphic_editor/NewProjectActivity.kt
@@ -1,9 +1,13 @@
 package com.hits.graphic_editor
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
 import com.hits.graphic_editor.databinding.ActivityNewProjectBinding
+import java.io.InputStream
 
 class NewProjectActivity : AppCompatActivity() {
 
@@ -18,5 +22,18 @@ class NewProjectActivity : AppCompatActivity() {
         val photo = intent?.getStringExtra("photo")
 
         binding.imageView.setImageURI(photo?.toUri())
+
     }
+
+
+    fun getBitmapFromUri(uri: Uri?): Bitmap? {
+    uri?.let{
+         val inputStream = contentResolver.openInputStream(uri) ?: return null
+         val bitmap = BitmapFactory.decodeStream(inputStream)
+         inputStream.close()
+         return  bitmap
+        }
+    return null
+    }
+
 }
