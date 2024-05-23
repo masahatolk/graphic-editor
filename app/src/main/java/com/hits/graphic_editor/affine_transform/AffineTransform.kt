7a1a8ccc69857ebc5f30bf4e7cac8d8cccb2b27d
Vasilii Polushkin <kvvarta@yandex.ru>
2024-05-24 02:36:10 +0700

    affine canvas

--- app/src/main/java/com/hits/graphic_editor/affine_transform/AffineTransform.kt
+++ app/src/main/java/com/hits/graphic_editor/affine_transform/AffineTransform.kt
@@ -2,17 +2,25 @@ package com.hits.graphic_editor.affine_transform
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.slider.Slider
+import com.hits.graphic_editor.custom_api.getBitMap
 import com.hits.graphic_editor.databinding.ActivityNewProjectBinding
 import com.hits.graphic_editor.databinding.AffineBottomMenuBinding
 import com.hits.graphic_editor.utils.Filter
 import com.hits.graphic_editor.utils.ProcessedImage
 import kotlinx.coroutines.runBlocking
+import kotlin.math.hypot
 
 class AffineTransform(
     private val context: Context,
@@ -40,7 +48,7 @@ class AffineTransform(
     private val cropSwitch = bottomMenu.cropSwitch
     private val ratioSlider = bottomMenu.aspectRatioSlider
 
-    private lateinit var pointTransfers: MutableList<PointTransfer>
+    private var pointTransfers: MutableList<PointTransfer> = mutableListOf()
 
     private var imageResult: AffineTransformedResult? = null
     private fun setListeners(){
@@ -83,116 +91,62 @@ class AffineTransform(
         }
     }
 
-
-
-
-    val pointerIds = mutableListOf(HASNT_OCCURRED, HASNT_OCCURRED,HASNT_OCCURRED)
-    val pointerStartXs = mutableListOf(0F, 0F, 0F)
-    val pointerStartYs = mutableListOf(0F, 0F, 0F)
-    val pointerEndXs = mutableListOf(0F, 0F, 0F)
-    val pointerEndYs = mutableListOf(0F, 0F, 0F)
+    private val canvasBitmap = Bitmap.createBitmap(
+        processedImage.getSimpleImage().width,
+        processedImage.getSimpleImage().height,
+        Bitmap.Config.ARGB_8888)
+    private val arrowsCanvas = Canvas(canvasBitmap)
+    private fun Canvas.drawArrow(tr: PointTransfer){
+        this.drawLine(tr.fromX, tr.fromY, tr.toX, tr.toY, Paint())
+    }
+    private fun Canvas.drawArrows(transfers: MutableList<PointTransfer>){
+        transfers.forEach { this.drawArrow(it) }
+    }
     @SuppressLint("ClickableViewAccessibility")
     private fun addGestures(){
+        var capturedPointIndex = 0
+        var startPointCaptured = false
+
         binding.root.setOnTouchListener{ _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
-                    if (pointerIds[0] == HASNT_OCCURRED) {
-                        pointerStartXs[0] = event.x
-                        pointerStartYs[0] = event.y
-                        pointerIds[0] = event.getPointerId(event.actionIndex)
-                    }
-                }
-                MotionEvent.ACTION_POINTER_DOWN -> {
-                    for (i in 1 until 3) {
-                        if (pointerIds[i] == HASNT_OCCURRED) {
-                            pointerStartXs[i] = event.x
-                            pointerStartYs[i] = event.y
-                            pointerIds[i] = event.getPointerId(event.actionIndex)
-                            break
-                        }
-                    }
-                }
-                MotionEvent.ACTION_POINTER_UP -> {
-                    for (i in 0 until 3) {
-                        if (pointerIds[i] == event.getPointerId(event.actionIndex)) {
-                            pointerEndXs[i] = event.x
-                            pointerEndYs[i] = event.y
 
-                            if (pointerIds.all { it != HASNT_OCCURRED }) {
-                                pointerIds.replaceAll { _ -> HASNT_OCCURRED }
-                                val transformedImage = runBlocking {
-                                    getAffineTransformedResult(
-                                        processedImage.getMipMapsContainer(),
-                                        PointTransfer(
-                                            pointerStartXs[0],
-                                            pointerEndXs[0],
-                                            pointerStartYs[0],
-                                            pointerEndYs[0]
-                                        ),
-                                        PointTransfer(
-                                            pointerStartXs[1],
-                                            pointerEndXs[1],
-                                            pointerStartYs[1],
-                                            pointerEndYs[1]
-                                        ),
-                                        PointTransfer(
-                                            pointerStartXs[2],
-                                            pointerEndXs[2],
-                                            pointerStartYs[2],
-                                            pointerEndYs[2]
-                                        )
-                                    )?.getSimpleImage()
-                                }
-                                if (transformedImage != null) {
-                                    processedImage.addToLocalStackAndSetImageToView(transformedImage)
-                                }
-                                else{
-                                    Toast.makeText(context, "No valid transform!", Toast.LENGTH_SHORT).show()
-                                }
-                            }
-                        }
-                    }
-                }
-                MotionEvent.ACTION_UP -> {
-                    for (i in 0 until 3) {
-                        if (pointerIds[i] == event.getPointerId(event.actionIndex)) {
-                            pointerEndXs[i] = event.x
-                            pointerEndYs[i] = event.y
+                    capturedPointIndex = 0
+                    startPointCaptured = false
 
-                            if (pointerIds.all { it != HASNT_OCCURRED }) {
-                                pointerIds.replaceAll { _ -> HASNT_OCCURRED }
-                                val transformedImage = runBlocking {
-                                    getAffineTransformedResult(
-                                        processedImage.getMipMapsContainer(),
-                                        PointTransfer(
-                                            pointerStartXs[0],
-                                            pointerEndXs[0],
-                                            pointerStartYs[0],
-                                            pointerEndYs[0]
-                                        ),
-                                        PointTransfer(
-                                            pointerStartXs[1],
-                                            pointerEndXs[1],
-                                            pointerStartYs[1],
-                                            pointerEndYs[1]
-                                        ),
-                                        PointTransfer(
-                                            pointerStartXs[2],
-                                            pointerEndXs[2],
-                                            pointerStartYs[2],
-                                            pointerEndYs[2]
-                                        )
-                                    )?.getSimpleImage()
-                                }
-                                if (transformedImage != null) {
-                                    processedImage.addToLocalStackAndSetImageToView(transformedImage)
-                                }
-                                else{
-                                    Toast.makeText(context, "No valid transform!", Toast.LENGTH_SHORT).show()
-                                }
-                            }
+                    for (i in 0 until pointTransfers.size){
+                        if (hypot(pointTransfers[i].fromX - event.x,
+                                pointTransfers[i].fromY - event.y) < 5) {
+                            capturedPointIndex = i
+                            startPointCaptured = true
+                            return@setOnTouchListener true
+                        }
+                        if (hypot(pointTransfers[i].toX - event.x,
+                                pointTransfers[i].toY - event.y) < 5) {
+                            capturedPointIndex = i
+                            startPointCaptured = false
+                            return@setOnTouchListener true
                         }
                     }
+
+                    pointTransfers.add(PointTransfer(event.x, event.y, event.x, event.y))
+
+                    if (pointTransfers.size >= 3)
+                        pointTransfers.removeFirst()
+                }
+                MotionEvent.ACTION_MOVE ->{
+                    if (startPointCaptured) {
+                        pointTransfers[capturedPointIndex].fromX = event.x
+                        pointTransfers[capturedPointIndex].fromY = event.y
+                    }
+                    else{
+                        pointTransfers[capturedPointIndex].toX = event.x
+                        pointTransfers[capturedPointIndex].toY = event.y
+                    }
+
+                    arrowsCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
+                    arrowsCanvas.drawArrows(pointTransfers)
+                    binding.extraImageView.setImageBitmap(canvasBitmap)
                 }
             }
             true
