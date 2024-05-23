74500a8b5839a9441f92e1415ff7ab3d3fbf9c5f
Vasilii Polushkin <kvvarta@yandex.ru>
2024-05-24 01:36:49 +0700

    affine with broken gestures

--- app/src/main/java/com/hits/graphic_editor/affine_transform/AffineTransform.kt
+++ app/src/main/java/com/hits/graphic_editor/affine_transform/AffineTransform.kt
@@ -4,8 +4,12 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.MotionEvent
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.slider.Slider
 import com.hits.graphic_editor.databinding.ActivityNewProjectBinding
+import com.hits.graphic_editor.databinding.AffineBottomMenuBinding
 import com.hits.graphic_editor.utils.Filter
 import com.hits.graphic_editor.utils.ProcessedImage
 import kotlinx.coroutines.runBlocking
@@ -16,23 +20,80 @@ class AffineTransform(
     override val layoutInflater: LayoutInflater,
     override val processedImage: ProcessedImage
 ):Filter {
+    companion object{
+        const val HASNT_OCCURRED= -1
+    }
     override fun onStart() {
+        addBottomMenu()
+        setListeners()
         addGestures()
     }
     @SuppressLint("ClickableViewAccessibility")
+    override fun onClose() {
+        binding.imageView.setOnTouchListener(null)
+        removeBottomMenu()
+    }
+    private val bottomMenu: AffineBottomMenuBinding by lazy {
+        AffineBottomMenuBinding.inflate(layoutInflater)
+    }
+    private val transformBtn = bottomMenu.transformButton
+    private val cropSwitch = bottomMenu.cropSwitch
+    private val ratioSlider = bottomMenu.aspectRatioSlider
+
+    private lateinit var pointTransfers: MutableList<PointTransfer>
+
+    private var imageResult: AffineTransformedResult? = null
+    private fun setListeners(){
+        ratioSlider.value = processedImage.getSimpleImage().width /
+                processedImage.getSimpleImage().height.toFloat()
+
+        transformBtn.setOnClickListener {
+            runBlocking {
+                imageResult = getAffineTransformedResult(
+                    processedImage.getMipMapsContainer(),
+                    pointTransfers[0],
+                    pointTransfers[1],
+                    pointTransfers[2],
+                    ratioSlider.value
+                )
+                if (imageResult != null) {
+                    processedImage.addToLocalStackAndSetImageToView(
+                        if (cropSwitch.isChecked) imageResult!!.getCropPreviewSimpleImage(ratioSlider.value)
+                        else imageResult!!.getSimpleImage()
+                    )
+                }
+            }
+        }
+        ratioSlider.addOnChangeListener() { _: Slider, value: Float, _: Boolean ->
+            if (!cropSwitch.isChecked) return@addOnChangeListener
+
+            if (imageResult != null) {
+                processedImage.addToLocalStackAndSetImageToView(
+                    imageResult!!.getCropPreviewSimpleImage(ratioSlider.value)
+                )
+            }
+        }
+        cropSwitch.setOnClickListener {
+            if (imageResult != null) {
+                processedImage.addToLocalStackAndSetImageToView(
+                    if (cropSwitch.isChecked) imageResult!!.getCropPreviewSimpleImage(ratioSlider.value)
+                    else imageResult!!.getSimpleImage()
+                )
+            }
+        }
+    }
+
+
+
+
+    val pointerIds = mutableListOf(HASNT_OCCURRED, HASNT_OCCURRED,HASNT_OCCURRED)
+    val pointerStartXs = mutableListOf(0F, 0F, 0F)
+    val pointerStartYs = mutableListOf(0F, 0F, 0F)
+    val pointerEndXs = mutableListOf(0F, 0F, 0F)
+    val pointerEndYs = mutableListOf(0F, 0F, 0F)
+    @SuppressLint("ClickableViewAccessibility")
     private fun addGestures(){
-        val HASNT_OCCURRED = -1
-
-        val pointerIds = mutableListOf(HASNT_OCCURRED, HASNT_OCCURRED,HASNT_OCCURRED)
-        val pointerStartXs = mutableListOf(0F, 0F, 0F)
-        val pointerStartYs = mutableListOf(0F, 0F, 0F)
-        val pointerEndXs = mutableListOf(0F, 0F, 0F)
-        val pointerEndYs = mutableListOf(0F, 0F, 0F)
-
-        //var isRunning = false
-        binding.imageView.setOnTouchListener{ _, event ->
-            //if (isRunning) return@setOnTouchListener true
-            //isRunning = true
+        binding.root.setOnTouchListener{ _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     if (pointerIds[0] == HASNT_OCCURRED) {
@@ -134,13 +195,23 @@ class AffineTransform(
                     }
                 }
             }
-            //isRunning = false
             true
         }
     }
-
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onClose() {
-        binding.imageView.setOnTouchListener(null)
+    private fun addBottomMenu() {
+        binding.root.addView(
+            bottomMenu.root.rootView,
+            ConstraintLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            ).apply {
+                bottomToBottom = binding.root.id
+                leftToLeft = binding.root.id
+                rightToRight = binding.root.id
+            }
+        )
+    }
+    private fun removeBottomMenu() {
+        binding.root.removeView(bottomMenu.root)
     }
 }
